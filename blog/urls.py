68d871686162be0bc3c7ecae7513abb591915fe7
Mau5trakt <kubzalemany@hotmail.com>
2024-03-15 10:19:19 -0600

    Day 4

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     #post views
 
     #functions from views
-    path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail')
+    #path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post-list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'),
 ]
